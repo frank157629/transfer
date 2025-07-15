b945fc8d75883e24a5787d090ec22dc921f7df40
frank157629 <haitian.liu@rwth-aachen.de>
2025-07-15 23:52:34 +0200

    update jobscript

--- PowerPINN/jobscript.sh
+++ PowerPINN/jobscript.sh
@@ -1,13 +1,12 @@
 #!/usr/local_rwth/bin/zsh
 ### SBATCH directives ###
-#SBATCH --account=rwth1854     # ← 换成自己的账号；没有就删除此行
-#SBATCH --partition=gpuv100            # GPU 队列名；CPU 调试可改 c23ms
 #SBATCH --gres=gpu:1                   # 1 块 GPU
-#SBATCH --mem=16G                      # 内存上限
+#SBATCH --mem=128G                      # 内存上限
 #SBATCH --time=12:00:00                # 12 小时
-#SBATCH --job-name=powerpinn           # 作业名称
+#SBATCH --job-name=BA_Haitian           # 作业名称
 #SBATCH --output=logs/%J.out           # 标准输出日志
 #SBATCH --error=logs/%J.err            # 标准错误日志
+#SBATCH --account=rwth1854
 ##########################
 
 # 1. 加载 CUDA 模块（与 PyTorch 版本匹配）
