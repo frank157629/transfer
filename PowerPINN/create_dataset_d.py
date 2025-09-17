aeef63313770b7147460ac30d4b72029b1563d60
frank157629 <haitian.liu@rwth-aachen.de>
2025-09-17 19:58:24 +0200

    Remove obsolete test_functions folder

--- PowerPINN/create_dataset_d.py
+++ PowerPINN/create_dataset_d.py
@@ -3,7 +3,6 @@ import torch
 import wandb
 import hydra
 from src.dataset.create_dataset_functions import ODE_modelling
-from src.ode.sm_models_d import SynchronousMachineModels
 from src.ode.gfl_models_d import GridFollowingConverterModels
 from omegaconf import OmegaConf
 
@@ -20,21 +19,8 @@ os.environ["HYDRA_FULL_ERROR"]="1"
 def main(config):
 
     theme = config.theme
-    if theme == "SM":
-        # Initialize wandb and log the dataset creation
-        cfg = OmegaConf.load("src/conf/setup_dataset_nn_sm.yaml.yaml")
-        cfg.theme = "SM"
-        run = wandb.init(project=cfg.wandb.project)
-        log_data_metrics_to_wandb(run, cfg)
-        print("Is cuda available?", torch.cuda.is_available())
-        SM_model = ODE_modelling(cfg) # Create an instance of the class ODE_modelling that creates the initial conditions and generates the dataset
-        init_conditions=SM_model.create_init_conditions_set3() # Define the initial conditions of the system
-        modelling_full=SynchronousMachineModels(cfg) # Define the SM models to be used
-        flag_for_time = True
-        solution = SM_model.solve_model(init_conditions, modelling_full, flag_for_time) # Solve the model for the various initial conditions
-        SM_model.save_dataset(solution) # Save the dataset
 
-    elif theme == "GFL":
+    if theme == "GFL":
         cfg = OmegaConf.load("src/conf/setup_dataset_gfl.yaml")
         cfg.theme = "GFL"
         run = wandb.init(project=cfg.wandb.project)
@@ -46,11 +32,6 @@ def main(config):
         flag_for_time = True  # we expect solution of each timestep
         solution = GFL_model.solve_model(init_conditions, modelling_full,flag_for_time)  # Solve the model for the various initial conditions
         GFL_model.save_dataset(solution)  # Save the dataset
-    elif theme == "GFM":
-        cfg = OmegaConf.load("src/conf/setup_dataset_gfm.yaml")
-        cfg.theme = "GFM"
-        #Add your specifications here...
-        raise NotImplementedError
     else:
         raise NotImplementedError
 
