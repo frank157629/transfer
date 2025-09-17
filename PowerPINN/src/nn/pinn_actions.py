70e68b1de3fc844fcad0e25e73b7c6c70e1dd421
frank157629 <haitian.liu@rwth-aachen.de>
2025-09-18 00:44:27 +0200

    Remove obsolete test_functions folder

--- PowerPINN/src/nn/pinn_actions.py
+++ PowerPINN/src/nn/pinn_actions.py
@@ -7,7 +7,6 @@ from src.nn.nn_dataset import DataSampler
 from src.nn.nn_model import Net, Network,PinnA, FullyConnectedResNet, Kalm
 from src.functions import *
 from src.nn.early_stopping import EarlyStopping
-from src.ode.sm_models_d import SynchronousMachineModels
 from src.ode.gfl_models_d import GridFollowingConverterModels
 import wandb
 import torch.autograd.functional as func
@@ -708,105 +707,7 @@ class PhysicsInformedNeuralNetworkActions():
             self.log_plot(y_pred, y_test, None, run, x_test,"test", starting_traj, total_traj)
         mae, rmse = self.loss_over_time(x_test, y_test, y_pred, run)
         return test_loss.item()
-    # #Haitian, temporary
-    # def log_plot(self, output, target, epoch, run, x_test, type,
-    #              starting_traj=0, total_traj=1):
-    #     # log in wandb
-    #     if self.cfg.theme == "SM":
-    #         modeling_guide_path = os.path.join(self.cfg.dirs.init_conditions_dir, "modellings_guide_sm.yaml")
-    #         modeling_guide = OmegaConf.load(modeling_guide_path)
-    #     elif self.cfg.theme == "GFL":
-    #         modeling_guide_path = os.path.join(self.cfg.dirs.init_conditions_dir, "modellings_guide_gfl.yaml")
-    #         modeling_guide = OmegaConf.load(modeling_guide_path)
-    #
-    #     # check if proposed modeling is in the modeling guide
-    #     for model in modeling_guide:
-    #         model_name = model.get("name")
-    #         if model_name == self.cfg.model.model_flag:
-    #             self.keys = model.get("keys")
-    #
-    #     pts_per_traj = int(self.data_loader.sample_per_traj)
-    #     max_traj = len(output) // pts_per_traj
-    #     print("type: ",str(type) + ", total_traj:" , str(max_traj) + ", max_traj", str(total_traj))
-    #     total_traj = min(total_traj, max_traj)
-    #
-    #     blk_idx = list(range(starting_traj, starting_traj + total_traj))
-    #     fig, axes = plt.subplots(total_traj, 3,figsize=(27, 3 * total_traj),sharex='col')
-    #     # …………………………………… 前面保持不变 ……………………………………
-    #
-    #     for r, k in enumerate(blk_idx):
-    #         lo, hi = k * pts_per_traj, (k + 1) * pts_per_traj
-    #
-    #         # -------- ① 反变换时间轴 --------
-    #         x_block_scaled = x_test[lo:hi]  # ← FIX: 传整块
-    #         x_block_phys = self.data_loader.detransform_input(x_block_scaled).detach()
-    #         t = x_block_phys[:, 0].cpu().numpy()  # ← FIX: 反变换后再取时间列
-    #
-    #         # -------- ② 反变换 δ / ω --------
-    #         y_true = self.data_loader.detransform_output(target[lo:hi]).detach().cpu().numpy()
-    #         y_pred = self.data_loader.detransform_output(output[lo:hi]).detach().cpu().numpy()
-    #         delta_true, omega_true = y_true[:, 0], y_true[:, 1]
-    #         delta_pred, omega_pred = y_pred[:, 0], y_pred[:, 1]
-    #
-    #         f_true = calculate_frequency(omega_true, np.pi * 100)
-    #         f_pred = calculate_frequency(omega_pred, np.pi * 100)
-    #
-    #         # -------- ④ 画图部分保持原逻辑 --------
-    #         # --- δ ---
-    #         if self.keys[0] == "delta":
-    #             axd = axes[r, 0]
-    #             axd.set_visible(True)
-    #             axd.plot(t, delta_true, color='tab:blue', lw=1.2,
-    #                      label='True' if r == 0 else None)
-    #             axd.plot(t, delta_pred, color='tab:orange', lw=1.2, ls='--',
-    #                      label='Pred' if r == 0 else None)
-    #             axd.grid(ls='--', alpha=.3)
-    #
-    #         # --- ω ---
-    #         if self.keys[1] == "omega":
-    #             axw = axes[r, 1]
-    #             axw.set_visible(True)
-    #             axw.plot(t, omega_true, color='tab:blue', lw=1.2,
-    #                      label='True' if r == 0 else None)
-    #             axw.plot(t, omega_pred, color='tab:orange', lw=1.2, ls='--',
-    #                      label='Pred' if r == 0 else None)
-    #             axw.grid(ls='--', alpha=.3)
-    #
-    #             # --- f ---
-    #             axf = axes[r, 2]
-    #             axf.set_visible(True)
-    #             axf.plot(t, f_true, color='tab:blue', lw=1.2,
-    #                      label='True' if r == 0 else None)
-    #             axf.plot(t, f_pred, color='tab:orange', lw=1.2, ls='--',
-    #                      label='Pred' if r == 0 else None)
-    #             axf.grid(ls='--', alpha=.3)
-    #
-    #         # 行标签
-    #         axd.text(-0.05, 0.5, f'traj {k + 1}',
-    #                  transform=axd.transAxes, va='center', ha='right',
-    #                  fontsize=9, weight='bold')
-    #         # label x-axes
-    #     axes[0, 0].set_title('δ')
-    #     axes[0, 1].set_title('ω')
-    #     axes[0, 2].set_title('f = ω/2*pi')
-    #     axes[-1, 0].set_xlabel('Time (s)')
-    #     axes[-1, 1].set_xlabel('Time (s)')
-    #     axes[-1, 2].set_xlabel('Time (s)')
-    #
-    #     #??
-    #     handles, labels = axes[0, 0].get_legend_handles_labels()
-    #     if handles:
-    #         fig.legend(handles, labels, loc='upper right')
-    #
-    #     plt.tight_layout()
-    #
-    #     # --------------------------- wandb docu.
-    #     gname = f"{type}{blk_idx[0]+1}-{blk_idx[-1]+1}"  # Example ：1-5, 6-10 …
-    #     run.log({f"traj_{gname}": wandb.Image(fig)},commit=False)
-    #
-    #     plt.close(fig)
-    #     run.log({}, commit=True)  #  ??
-        # …………………………………… 后面保持不变 ……………………………………
+
     # Haitian, added different logic for plotting GFL
     def log_plot(self, output, target, epoch, run, x_test, type, starting_traj=0, total_traj=1):
         # log in wandb
