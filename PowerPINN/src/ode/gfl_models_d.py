430173a8f8db7c61e8a97827a7952a97338997a1
frank157629 <haitian.liu@rwth-aachen.de>
2025-09-18 02:02:49 +0200

    Remove obsolete test_functions folder

--- PowerPINN/src/ode/gfl_models_d.py
+++ PowerPINN/src/ode/gfl_models_d.py
@@ -84,18 +84,6 @@ class GridFollowingConverterModels:
             for param in ['S_b','V_g','V_dc','f_0','w_g','T_s','X_Lg','R_Lg','L_Lg','X_R_ratio','i_d_c','i_q_c','K_p','K_i']:
                 setattr(self, param, getattr(model_params, param))
 
-        if self.model_flag == 'GFL_4th_order':
-            self.model_name = "Reduced-order model for GFL_4th_order control with four states"
-            for param in ["Pe_kw", "U_f", "V_dc", "f_e","w_g", "L", "L_g", "K_p", "K_i", "I_d_ref", "I_q_ref", "K_pc","K_ic"]:
-                setattr(self, param, getattr(model_params, param))
-
-
-        if self.model_flag == 'GFL_7th_order':
-            self.model_name = "Full 7-state grid-connected converter model with GFL_2nd_order, filter inductor & capacitor dynamics"
-            for param in ["S_b", "V_g", "V_dc", "f_0", "w_g", "T_s", "L_g", "R_g", "C_f", "k_p", "k_i", "w_n", "v_gd_g", "v_gq_g", "i_Ld_g", "i_Lq_g"]:
-                setattr(self, param, getattr(model_params, param))
-
-
         return
     #Haitian, to prevent faults during solving the ODE
     import numpy as np
@@ -135,74 +123,6 @@ class GridFollowingConverterModels:
             domega_dt = (T_m - T_e -D * omega ) * (1/M)
             return [ddelta_dt, domega_dt]
 
-        elif self.model_flag == "GFL_4th_order":
-            delta, delta_omega, delta_Id, delta_Id_dt = x
-            delta_dt = delta_omega
-
-
-            den = (1 - self.K_p * self.L_g * (self.I_d_ref + delta_Id))
-            check_state_sanity(t, x, den)
-
-            if isinstance(delta, torch.Tensor):
-                delta_dt = delta_omega
-                delta_omega_dot = (
-                        (self.K_p
-                        * (-self.U_f * delta_omega * torch.cos(delta)
-                        + (self.w_g + delta_omega) * self.L_g * delta_Id_dt)
-                        + self.K_i
-                        * (-self.U_f * torch.sin(delta)
-                        - self.L_g * (delta_omega + self.w_g)
-                        * (self.I_d_ref + delta_Id))
-                        ) / (1 - self.K_p * self.L_g * (self.I_d_ref + delta_Id))
-                )
-
-                delta_Id_ddt = (
-                                - self.K_pc * delta_Id_dt
-                                - self.K_ic * delta_Id
-                                + self.U_f * delta_omega * torch.sin(delta)
-                                ) / self.L
-
-            else:
-                delta_omega_dot = (
-                        (self.K_p
-                        * (-self.U_f * delta_omega * np.cos(delta)
-                        + (self.w_g + delta_omega) * self.L_g * delta_Id_dt)
-                        + self.K_i
-                        * (-self.U_f * np.sin(delta)
-                        - self.L_g * (delta_omega + self.w_g)
-                        * (self.I_d_ref + delta_Id))
-                        ) / (1 - self.K_p * self.L_g * (self.I_d_ref + delta_Id))
-                )
-
-                delta_Id_ddt = (
-                                - self.K_pc * delta_Id_dt
-                                - self.K_ic * delta_Id
-                                + self.U_f * delta_omega * np.sin(delta)
-                               ) / self.L
-
-            # four-state derivative list
-            return [delta_dt,  delta_omega_dot, delta_Id_dt, delta_Id_ddt]
-
-        elif self.model_flag == "GFL_7th_order":
-            gamma, delta, theta_pll, i_gd_g, i_gq_g, v_od_g, v_oq_g = x
-            if isinstance(delta, torch.Tensor):
-                dgamma_dt = -torch.sin(delta) * v_od_g + torch.cos(delta)*v_oq_g
-                ddelta_dt = -self.k_p*torch.sin(delta)*v_od_g+self.k_p*torch.cos(delta)*v_oq_g+self.k_i*gamma+self.w_n-self.w_g
-                dtheta_pll_dt = -self.k_p*torch.sin(delta)*v_od_g+self.k_p*torch.cos(delta)*v_oq_g+self.k_i*gamma+self.w_n
-                di_gd_g_dt = (1/self.L_g)*(v_od_g-self.v_gd_g-self.R_g*i_gd_g+self.w_n*self.L_g*i_gq_g)
-                di_gq_g_dt = (1/self.L_g)*(v_oq_g-self.v_gq_g-self.R_g*i_gq_g-self.w_n*self.L_g*i_gd_g)
-                dv_od_g_dt = (1/self.C_f)*(self.i_Ld_g-i_gd_g+self.w_n*self.C_f*v_oq_g)
-                dv_oq_g_dt = (1/self.C_f)*(self.i_Lq_g-i_gq_g-self.w_n*self.C_f*v_od_g)
-            else:
-                dgamma_dt = -np.sin(delta) * v_od_g + np.cos(delta) * v_oq_g
-                ddelta_dt = -self.k_p * np.sin(delta) * v_od_g + self.k_p * np.cos(delta) * v_oq_g + self.k_i * gamma + self.w_n - self.w_g
-                dtheta_pll_dt = -self.k_p * np.sin(delta) * v_od_g + self.k_p * np.cos(delta) * v_oq_g + self.k_i * gamma + self.w_n
-                di_gd_g_dt = (1 / self.L_g) * (v_od_g - self.v_gd_g - self.R_g * i_gd_g + self.w_n * self.L_g * i_gq_g)
-                di_gq_g_dt = (1 / self.L_g) * (v_oq_g - self.v_gq_g - self.R_g * i_gq_g - self.w_n * self.L_g * i_gd_g)
-                dv_od_g_dt = (1 / self.C_f) * (self.i_Ld_g - i_gd_g + self.w_n * self.C_f * v_oq_g)
-                dv_oq_g_dt = (1 / self.C_f) * (self.i_Lq_g - i_gq_g - self.w_n * self.C_f * v_od_g)
-
-            return [dgamma_dt, ddelta_dt, dtheta_pll_dt, di_gd_g_dt, di_gq_g_dt, dv_od_g_dt, dv_oq_g_dt]
         else:
             raise NotImplementedError
 
